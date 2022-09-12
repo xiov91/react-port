a9e43a1a41cc61966dc80f7f206238e8124e4aac
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-09-12 10:20:19 -0400

    setup and component start

---
+++ src/components/About/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section>
+            <h1 id="about">Who am I?</h1>
+            <p>
+                I'm Xiomara--that's Zee for those who don't like a mouthful. I'm nearly at the end of my Full Stack Coding Bootcamp,
+                and I'm loving all the different things I've been able to learn. I'm excited to continue the trend and branch out into Python
+                and C# to expand my skills and continue down my path of both web development and game development.
+            </p>
+        </section>
+    );
+}
+
+export default About;
