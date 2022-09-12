c03731f9ae5742f211854144255de040f5c88f78
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-09-12 10:43:26 -0400

    added bootstrap & imported CSS from old portfolio

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -60,6 +60,33 @@ function Nav() {
                     <li>
                         <span>Portfolio</span>
                     </li>
+{/*                     <li>
+                        <div className="dropdown">
+                                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-togle="dropwdown" aria-haspopup="true" aria-expanded="false">
+                                <span>Portfolio</span>
+                                </button>
+                        </div>
+                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a href="#" className="dropdown-item">
+                                Run Buddy
+                            </a>
+                            <a href="#" className="dropdown-item">
+                                Furever Pets
+                            </a>
+                            <a href="#" className="dropdown-item">
+                                Readme Generator
+                            </a>
+                            <a href="#" className="dropdown-item">
+                                Make Tracks
+                            </a>
+                            <a href="#" className="dropdown-item">
+                                Thoughts For Your Thoughts
+                            </a>
+                            <a href="#" className="dropdown-item">
+                                Regex Tutorial
+                            </a>
+                        </div>
+                    </li> */}
                 </ul>
             </nav>
 
